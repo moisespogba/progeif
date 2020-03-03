343ca187d1ee4dfd4b30d5e894bfa6c2b9050f67
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2020-03-03 10:38:55 -0300

    primeiro commit

---
+++ model/aluno.js
@@ -0,0 +1,16 @@
+var conexao = require('./conexao')
+
+var AlunoSchema = new conexao.Schema({
+    nome:{
+        type:String
+    },
+    endereco:{
+        type:String
+    },
+    telefone:{
+        type:String
+    }
+})
+
+module.exports = conexao.model('Aluno',AlunoSchema)
+
